82c94dd0e6e2d19e3412e8d4d5581813fb25def1
Reza Esfandiari <resfandiari.dev@gmail.com>
2023-12-24 12:49:57 +0330

    Implement Async Image

---
+++ 4-AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Reza Esfandiari on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 4-AsyncImage/AsyncImage/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  AsyncImage
+//
+//  Created by Reza Esfandiari on 12/23/23.
+//
+
+import SwiftUI
+
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
+struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    var body: some View {
+        
+        
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .imageModifier()
+                // .transition(.move(edge: .bottom))
+                // .transition(.slide)
+                    .transition(.scale)
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
+            }
+        }
+        .padding(40)
+    }
+}
+
+#Preview {
+    ContentView()
+}
