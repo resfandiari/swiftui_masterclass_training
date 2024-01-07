487ba85bb5621650b001d635be69686806d977d3
Reza Esfandiari <resfandiari.dev@gmail.com>
2024-01-07 15:15:39 +0330

    Implement Guess the flag project

---
+++ 5-GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Reza Esfandiari on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
