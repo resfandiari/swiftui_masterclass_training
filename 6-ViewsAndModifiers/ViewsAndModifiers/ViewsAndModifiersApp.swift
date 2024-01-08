f2c6f95c180d2f8a675c755d452d48744e7006ad
Reza Esfandiari <resfandiari.dev@gmail.com>
2024-01-08 15:22:23 +0330

    Implement View and modifiers

---
+++ 6-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Reza Esfandiari on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
