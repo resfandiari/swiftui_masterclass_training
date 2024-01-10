3742bbafbf245f33b1a4e4d74268547bc21f49af
Reza Esfandiari <resfandiari.dev@gmail.com>
2024-01-10 13:50:28 +0330

    Implement Better rest project

---
+++ 7-BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Reza Esfandiari on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
